9d0774e55fdb5cae858060f1c90443b3f57e2986
Dvoctz <dvoctz@zap.co.tz>
2025-11-17 18:49:53 +0300

    feat: Update Tailwind CSS content paths
    Adjusted Tailwind CSS configuration to include new directories for content scanning. This ensures that styles are correctly applied to components, views, context, data, and API files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{ts,tsx}",
+    "./{components,views,context,data,api}/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
