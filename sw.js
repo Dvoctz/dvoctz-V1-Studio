5689a5cb25fce4fa1af48c51948b3d9745f217d6
Dvoctz <dvoctz@zap.co.tz>
2025-12-06 14:48:33 +0300

    feat: Add canvas-confetti and update service worker cache
    Integrates the `canvas-confetti` library for celebratory animations and updates the service worker cache version to ensure all users receive the latest assets.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'dvoc-tanzania-cache-v7.2.6'; // Increment version to force update & clear old caches
+const CACHE_NAME = 'dvoc-tanzania-cache-v7.3.0'; // Increment version to force update & clear old caches
 const APP_SHELL_URLS = [
   '/',
   '/index.html',
